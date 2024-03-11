95bb64dc532a6d71d78d2b460b5acb66258611f2
kakashi <trishiragolder69@gmail.com>
2024-03-11 21:26:18 +0530

    removed: broken code

--- lib/thread/macros.ts
+++ lib/thread/macros.ts
@@ -1,13 +1,16 @@
 // deno-lint-ignore-file
+import { $unimplemented } from "../declarative-macros/mod.ts";
 import { Thread } from "./thread.ts";
-import { AsyncPropertyDescriptor } from "../proc-macros/types.ts";
 
 
-
-export function thread<T>(_target: Object,_key: keyof any,descriptor: AsyncPropertyDescriptor<T>) {
-  descriptor.value=async function(...args: unknown[]) {
-    return Thread.spawn(()=> descriptor.value!(...args));
-  };
-  return descriptor;
+/**
+ * Makes the function spawn a call itself in a different thread.
+ */
+export function thread<T>(_target: Object,_key: keyof any,descriptor: PropertyDescriptor): PropertyDescriptor {
+  // descriptor.value=function(...args: unknown[]) {
+  //   return Thread.spawn<T>(()=> descriptor.value!(...args));
+  // };
+  // return descriptor;
+  return $unimplemented();
 };
 
