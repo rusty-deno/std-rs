07b9028d7fcb4c165ad53f135bd6f391edba6ce0
kakashi <trishiragolder69@gmail.com>
2024-03-11 17:30:54 +0530

    fixed module

--- lib/thread/thread.ts
+++ lib/thread/thread.ts
@@ -1,10 +1,22 @@
-import { Drop } from '../drop.ts';
-import { Some,None,Option } from "../../std/mod.ts";
-import * as lib from '../../bindings/std_rs.js';
 import { Fn } from '../types.ts';
-import { $todo } from "../declarative-macros/mod.ts";
+import { Drop } from '../drop.ts';
+import * as lib from '../../bindings/std_rs.js';
+import { Some,None,Option,result } from "../../std/mod.ts";
 
 
+
+// TODO: seal the class/**
+/**
+ * A handle to a thread.
+ * Threads are represented via the {@linkcode Thread} type, which you can get in one of two ways:
+ * 
+ * * By spawning a new thread, e.g., using the {@linkcode Thread.spawn} function, and calling the {@linkcode JoinHandle.thread} on the {@linkcode JoinHandle}.
+ * * By requesting the current thread, using the {@linkcode Thread.current} function.
+ * 
+ * The {@linkcode Thread.current} function is available even for threads not spawned by the APIs of this module.
+ * 
+ * There is usually no need to create a {@linkcode Thread} object yourself, one should instead use a function like {@linkcode Thread.spawn} to create new threads, see the docs of Builder and spawn for more details.
+ */
 export class Thread extends Drop {
   public readonly id: bigint;
   public readonly name: Option<string>;
@@ -24,13 +36,57 @@ export class Thread extends Drop {
     const fn=Deno.UnsafeCallback.threadSafe({
       parameters: [],
       result: "void"
-    },()=> {
-      retVal=Some(f());
-    });
-    
-    return $todo();
+    },()=> retVal=Some(f()));
+
+    return JoinHandleBuilder.new(
+      lib.spawn_thread(
+        Number(Deno.UnsafePointer.value(fn.pointer))
+      ),
+      retVal
+    );
   }
 }
+
+// TODO: seal the class
+export class JoinHandle<T> extends Drop {
+  protected constructor(private ___ptr: number,private ___return: Option<T>) {
+    super();
+  }
+
+  protected drop(): void {
+    lib.drop_join_handle(this.___ptr);
+  }
+
+  public isFinished() {
+    return lib.is_finished(this.___ptr);
+  }
+
+  public thread() {
+    return ThreadHandleBuilder.new(lib.thread(this.___ptr));
+  }
+
+  public join() {
+    return result.$resultSync(()=> {
+      lib.join(this.___ptr);
+      return this.___return.value as T;
+    });
+  }
+}
+
+class JoinHandleBuilder<_> extends JoinHandle<_> {
+  public static new<T>(ptr: number,_return: Option<T>) {
+    return new JoinHandle<T>(ptr,_return);
+  }
+}
+
+class ThreadHandleBuilder extends Thread {
+  public static new(ptr: number) {
+    return new Thread(ptr);
+  }
+}
+
+
+
 
 
 
