dc1526aa3f9608f7dc540337d16faba6449e347b
kakashi <trishiragolder69@gmail.com>
2024-03-13 23:03:37 +0530

    fixed Clone

--- lib/proc-macros/clone.ts
+++ lib/proc-macros/clone.ts
@@ -1,11 +1,12 @@
-// deno-lint-ignore-file
+// deno-lint-ignore no-unused-vars
 import { Clone } from "../clone.ts";
 import { Class } from "./types.ts";
 
 
 
 
-export function Clone<C extends Class>(constructor: C) {
+// deno-lint-ignore no-explicit-any
+export function Clone<C extends Class>(constructor: C,_: any) {
   return class extends constructor implements Clone {
     clone(): C {
       return structuredClone(this);
