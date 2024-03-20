8841a3e2278641d6fa45dc1ccc5d839da9fb6602
kakashi <trishiragolder69@gmail.com>
2024-03-20 18:19:13 +0530

    implemented FsFile

--- lib/fs/file.ts
+++ lib/fs/file.ts
@@ -1,8 +1,9 @@
 import { $result,$resultSync } from "../error/result/mod.ts";
-import { $option,$optionSync } from "../error/option/mod.ts";
+import { SeekFrom } from "./types.ts";
+import { FileTimes } from "./types.ts";
 
 export class FsFile {
-  constructor(private inner: Deno.FsFile) {};
+  private constructor(private inner: Deno.FsFile) {};
   
   public static create(path: string) {
     return $result(async ()=> new FsFile(await Deno.create(path)));
@@ -40,6 +41,7 @@ export class FsFile {
    * ### Examples
   ```ts
   import { FsFile } from "@std/fs";
+import { FileTimes, SeekFrom } from './types';
   
   const encoder = new TextEncoder();
   using file = await FsFile.create("foo.txt").unwrap();
@@ -179,12 +181,12 @@ export class FsFile {
   ```ts
   import { FsFile } from "@std/fs";
 
-  const file = await FsFile.open("foo.txt")?;
+  using file = await FsFile.open("foo.txt")?;
   const metadata = await file.metadata()?;
   ```
    */
   public metadata() {
-    return $result(async ()=>  await this.inner.stat());
+    return $result(()=> this.inner.stat());
   }
 
   /**
@@ -194,7 +196,7 @@ export class FsFile {
   ```ts
   import { FsFile } from "@std/fs";
 
-  const file = FsFile.openSync("foo.txt")?;
+  using file = FsFile.openSync("foo.txt")?;
   const metadata = file.metadataSync()?;
   ```
    */
@@ -203,37 +205,362 @@ export class FsFile {
   }
 
 
+  /**
+   * Changes the timestamps of the underlying file.
+   * 
+   * ### Errors
+   * * This function will return an error if the user lacks permission to change timestamps on the underlying file. It may also return an error in other os-specific unspecified cases.
+   * * This function may return an error if the operating system lacks support to change one or more of the timestamps set in the FileTimes structure.
+   * 
+   * ### Examples
+  ```ts
+  import { FsFile } from "@std/fs";
+  
+  const { accessed,modified } = await fs.metadata("src")?;
+  const dest = await File.open("dest")?;
 
-  public async read() {
-    return await $option(async ()=> {
-      const buf=new Uint8Array;
-      const bytes=await this.inner.read(buf);
-      return bytes==null?bytes:buf;
+  await dest.setTimes({ accessed, modified })?;
+  ```
+   */
+  public setTimes(times: FileTimes) {this.metadataSync().unwrap().atime;
+    return $result(()=> this.inner.utime(times.accessed,times.modified));
+  }
+
+  /**
+   * Changes the timestamps of the underlying file synchronously.
+   * 
+   * ### Errors
+   * * This function will return an error if the user lacks permission to change timestamps on the underlying file. It may also return an error in other os-specific unspecified cases.
+   * * This function may return an error if the operating system lacks support to change one or more of the timestamps set in the FileTimes structure.
+   * 
+   * ### Examples
+  ```ts
+  import { FsFile } from "@std/fs";
+  
+  const { accessed,modified } = fs.metadataSync("src")?;
+  const dest = File.openSync("dest")?;
+
+  dest.setTimesSync({ accessed, modified })?;
+  ```
+   */
+  public setTimesSync(times: FileTimes) {
+    return $resultSync(()=> this.inner.utimeSync(times.accessed,times.modified));
+  }
+
+  /**
+   * Changes the modification time of the underlying file.
+   * 
+   * This is an alias for
+  ```ts
+  await file.setTimes({ accessed, modified })?;
+  ```
+   */
+  public setModified(modified: number|Date) {
+    return $result(async ()=> {
+      const accessed=(await this.inner.stat()).atime;
+      if(accessed===null) throw "couldn't fetch access-time";
+
+      await this.inner.utime(accessed,modified);
     });
   }
 
+  /**
+   * Changes the modification time of the underlying file.
+   * 
+   * This is an alias for
+  ```ts
+  file.setTimesSync({ accessed, modified })?;
+  ```
+   */
+  public setModifiedSync(modified: number|Date) {
+    return $resultSync(()=> {
+      const accessed=this.inner.statSync().atime;
+      if(accessed===null) throw "couldn't fetch access-time";
+
+      this.inner.utime(accessed,modified);
+    });
+  }
+
+  /**
+   * Read the file into an array buffer (p).
+   * Resolves to the number of bytes read during the operation.
+   * 
+   * **NOTE**: It is not guaranteed that the full buffer will be read in a single call.
+   * 
+   * ### Errors
+   * * EOF (end of file)
+   * * An error of the {@linkcode ErrorKind.Interrupted} kind is non-fatal and the write operation should be retried if there is nothing else to do.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+  
+  using file = await FsFile.open("/foo/bar.txt")?;
+  const buf = new Uint8Array(100);
+  const numberOfBytesRead = await file.read(buf)?;// 11 bytes
+  const text = new TextDecoder().decode(buf);// "hello world"
+  ```
+  * * It is possible for a read to successfully return with 0 bytes. This does not indicate EOF.
+   */
+  public read(buf: Uint8Array) {
+    return $result(async ()=> {
+      const len=await this.inner.read(buf);
+      if(len===null) throw "EOF";
+
+      return len;
+    });
+  }
+
+  /**
+   * Read the file into an array buffer (p) synchrounously.
+   * Resolves to the number of bytes read during the operation.
+   * 
+   * **NOTE**: It is not guaranteed that the full buffer will be read in a single call.
+   * 
+   * ### Errors
+   * * EOF (end of file)
+   * * An error of the {@linkcode ErrorKind.Interrupted} kind is non-fatal and the write operation should be retried if there is nothing else to do.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+  
+  using file = FsFile.openSync("/foo/bar.txt")?;
+  const buf = new Uint8Array(100);
+  const numberOfBytesRead = file.readSync(buf)?;// 11 bytes
+  const text = new TextDecoder().decode(buf);// "hello world"
+  ```
+  * * It is possible for a read to successfully return with 0 bytes. This does not indicate EOF.
+   */
   public readSync() {
-    return $optionSync(()=> {
+    return $resultSync(()=> {
       const buf=new Uint8Array;
       const bytes=this.inner.readSync(buf);
       return bytes==null?bytes:buf;
     });
   }
 
+  /**
+   * Write the contents of the array buffer (p) to the file.
+   * Resolves to the number of bytes written.
+   * 
+   * **NOTE**: It is not guaranteed that the full buffer will be written in a single call.
+   * 
+   * ### Errors
+   * * Each call to write may generate an I/O error indicating that the operation could not be completed.
+   * If an error is returned then no bytes in the buffer were written to this writer.
+   * * It is not considered an error if the entire buffer could not be written to this writer.
+   * * An error of the {@linkcode ErrorKind.Interrupted} kind is non-fatal and the write operation should be retried if there is nothing else to do.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+
+  const encoder = new TextEncoder();
+  const data = encoder.encode("Hello world");
+
+  using file = await FsFile.open("/foo/bar.txt", { write: true })?;
+  const bytesWritten = await file.write(data)?; // 11
+  ```
+   */
   public write(buf: Uint8Array) {
     return $result(()=> this.inner.write(buf));
   }
 
+  /**
+   * Write the contents of the array buffer (p) to the file synchronously.
+   * Resolves to the number of bytes written.
+   * 
+   * **NOTE**: It is not guaranteed that the full buffer will be written in a single call.
+   * 
+   * ### Errors
+   * * Each call to write may generate an I/O error indicating that the operation could not be completed.
+   * If an error is returned then no bytes in the buffer were written to this writer.
+   * * It is not considered an error if the entire buffer could not be written to this writer.
+   * * An error of the {@linkcode ErrorKind.Interrupted} kind is non-fatal and the write operation should be retried if there is nothing else to do.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+
+  const encoder = new TextEncoder();
+  const data = encoder.encode("Hello world");
+
+  using file = FsFile.openSync("/foo/bar.txt", { write: true })?;
+  const bytesWritten = file.writeSync(data)?; // 11
+  ```
+   */
   public writeSync(buf: Uint8Array) {
     return $resultSync(()=> this.inner.writeSync(buf));
   }
 
-  public seek(offset: number|bigint,whench: Deno.SeekMode) {
-    return this.inner.seek(offset,whench);
+  /**
+   * Seek to an offset, in bytes, in a stream.
+   * 
+   * A seek beyond the end of a stream is allowed, but behavior is defined by the implementation.
+   * 
+   * If the seek operation completed successfully, this method returns the new position from the start of the stream.
+   * That position can be used later with {@linkcode SeekFrom.Start}.
+   * 
+   * ### Errors
+   * * Seeking can fail, for example because it might involve flushing a buffer.
+   * * Seeking to a negative offset is considered an error.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile,SeekFrom } from "@std/fs";
+
+  // Given file pointing to file with "Hello world", which is 11 bytes long:
+  using file = await FsFile.open("hello.txt",{ read: true, write: true, truncate: true, create: true })?;
+  await file.write(new TextEncoder().encode("Hello world"))?;
+
+  // advance cursor 6 bytes
+  const cursorPosition = await file.seek(6, SeekFrom.Start)?;
+  console.log(cursorPosition);// 6
+  const buf = new Uint8Array(100);
+  await file.read(buf)?;
+  console.log(new TextDecoder().decode(buf));// "world"
+  ```
+   * 
+   * The seek modes work as follows:
+   * 
+   * 
+  ```ts
+  import { FsFile,SeekFrom } from "@std/fs";
+
+  // Given file.rid pointing to file with "Hello world", which is 11 bytes long:
+  const file = await FsFile.open("hello.txt",{ read: true, write: true, truncate: true, create: true })?;
+  await file.write(new TextEncoder().encode("Hello world"))?;
+
+  // Seek 6 bytes from the start of the file
+  console.log(await file.seek(6, SeekFrom.Start)?);// "6"
+  // Seek 2 more bytes from the current position
+  console.log(await file.seek(2, SeekFrom.Current)?);// "8"
+  // Seek backwards 2 bytes from the end of the file
+  console.log(await file.seek(-2, SeekFrom.End)?);// "9" (i.e. 11-2)
+  ```
+   */
+  public seek(offset: number|bigint,whench: SeekFrom) {
+    return $result(()=> this.inner.seek(offset,whench));
   }
 
+  /**
+   * Seek to an offset, in bytes, in a stream synchronously.
+   * 
+   * A seek beyond the end of a stream is allowed, but behavior is defined by the implementation.
+   * 
+   * If the seek operation completed successfully, this method returns the new position from the start of the stream.
+   * That position can be used later with {@linkcode SeekFrom.Start}.
+   * 
+   * ### Errors
+   * * Seeking can fail, for example because it might involve flushing a buffer.
+   * * Seeking to a negative offset is considered an error.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile,SeekFrom } from "@std/fs";
 
+  // Given file pointing to file with "Hello world", which is 11 bytes long:
+  using file = FsFile.openSync("hello.txt",{ read: true, write: true, truncate: true, create: true })?;
+  file.writeSync(new TextEncoder().encode("Hello world"))?;
 
+  // advance cursor 6 bytes
+  const cursorPosition = file.seekSync(6, SeekFrom.Start)?;
+  console.log(cursorPosition);// 6
+  const buf = new Uint8Array(100);
+  file.readSync(buf)?;
+  console.log(new TextDecoder().decode(buf));// "world"
+  ```
+   * 
+   * The seek modes work as follows:
+   * 
+   * 
+  ```ts
+  import { FsFile,SeekFrom } from "@std/fs";
+
+  // Given file.rid pointing to file with "Hello world", which is 11 bytes long:
+  const file = FsFile.openSync("hello.txt",{ read: true, write: true, truncate: true, create: true })?;
+  file.writeSync(new TextEncoder().encode("Hello world"))?;
+
+  // Seek 6 bytes from the start of the file
+  console.log(file.seekSync(6, SeekFrom.Start)?);// "6"
+  // Seek 2 more bytes from the current position
+  console.log(file.seekSync(2, SeekFrom.Current)?);// "8"
+  // Seek backwards 2 bytes from the end of the file
+  console.log(file.seekSync(-2, SeekFrom.End)?);// "9" (i.e. 11-2)
+  ```
+   */
+  public seekSync(offset: number,whench: SeekFrom) {
+    return $resultSync(()=> this.inner.seekSync(offset,whench));
+  }
+
+  /**
+   * Checks if the file resource is a TTY (terminal).
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+
+  // This example is system and context specific
+  using file = await FsFile.open("/dev/tty6")?;
+  file.isTerminal();// Ok(true)
+  ```
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public isTerminal() {
+    return $resultSync(()=> this.inner.isTerminal());
+  }
+
+  /**
+   * Set TTY to be under raw mode or not.
+   * In raw mode, characters are read and returned as is, without being processed.
+   * All special processing of characters by the terminal is disabled, including echoing input characters.
+   * Reading from a TTY device in raw mode is faster than reading from a TTY device in canonical mode.
+   * 
+   * ### Example
+  ```ts
+  import { FsFile } from "@std/fs";
+
+  using file = await Deno.open("/dev/tty6");
+  file.setRaw(true, { cbreak: true });
+  ```
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public setRaw(mode: boolean,options?: Deno.SetRawOptions) {
+    return $resultSync(()=> this.inner.setRaw(mode,options));
+  }
+
+  /**
+   * Acquire an advisory file-system lock for the file.
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public lock(exclusive?: boolean) {
+    return $result(()=> this.inner.lock(exclusive));
+  }
+
+  /**
+   * Acquire an advisory file-system lock for the file synchronously.
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public lockSync(exclusive?: boolean) {
+    return $resultSync(()=> this.inner.lockSync(exclusive));
+  }
+
+  /**
+   * Release an advisory file-system lock for the file.
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public unlock() {
+    return $result(()=> this.inner.unlock());
+  }
+
+  /**
+   * Release an advisory file-system lock for the file synchronously.
+   * * **UNSTABLE**: New API, yet to be vetted.
+   */
+  public unlockSync() {
+    return $resultSync(()=> this.inner.unlockSync());
+  }
 }
 
 
