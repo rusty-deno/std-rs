f447ce7f1c920ed018fcd2e2d316544abdf882d0
kakashi <trishiragolder69@gmail.com>
2024-03-24 22:12:58 +0530

    implemented metadata tyoe

---
+++ lib/fs/_metadata.ts
@@ -0,0 +1,51 @@
+import { Metadata } from "../../std/fs.ts";
+import { Option } from "../error/mod.ts";
+
+
+export function parseMetadata({
+  atime,
+  birthtime,
+  blksize,
+  blocks,
+  dev,
+  gid,
+  ino,
+  isBlockDevice,
+  isCharDevice,
+  isDirectory,
+  isFifo,
+  isFile,
+  isSocket,
+  isSymlink,
+  mode,
+  mtime,
+  nlink,
+  rdev,
+  size,
+  uid
+}: Deno.FileInfo): Metadata {
+  return {
+    dev,
+    size,
+    isFile,
+    isSymlink,
+    isDirectory,
+    atime: new Option(atime),
+    birthtime: new Option(birthtime),
+    blksize: new Option(blksize),
+    blocks: new Option(blocks),
+    gid: new Option(gid),
+    ino: new Option(ino),
+    isBlockDevice: new Option(isBlockDevice),
+    isCharDevice: new Option(isCharDevice),
+    isFifo: new Option(isFifo),
+    isSocket: new Option(isSocket),
+    mode: new Option(mode),
+    mtime: new Option(mtime),
+    nlink: new Option(nlink),
+    rdev: new Option(rdev),
+    uid: new Option(uid)
+  };
+}
+
+
