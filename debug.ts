1d1d81112223be9ee639937c7e8ed258b29f686f
kakashi <trishiragolder69@gmail.com>
2024-04-03 20:08:36 +0530

    fixed Eq and PartialEq

--- debug.ts
+++ debug.ts
@@ -1,1 +1,18 @@
+import { PartailEq } from './lib/cmp/eq.ts';
+
+class XD implements PartailEq<XD|number> {
+  constructor(public xd: number) {}
+  
+  public eq(rhs: XD|number): boolean {
+    return rhs instanceof XD?this.xd===rhs.xd:this.xd===rhs;
+  }
+}
+
+class XDXD {
+  constructor(public xd: number) {}
+}
+
+const xd=new XD(69);
+
+console.log(xd.eq(new XDXD(69) as XD));
 
