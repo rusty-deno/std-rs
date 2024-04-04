273e6f658d0b7f20118d01b62af33a7d21005206
kakashi <trishiragolder69@gmail.com>
2024-04-05 00:55:01 +0530

    added promise macro

--- lib/declarative-macros/mod.ts
+++ lib/declarative-macros/mod.ts
@@ -2,3 +2,10 @@ export * from "./collections.ts";
 export * from "./panics.ts";
 export * from "./net.ts";
 export * from "./test/mod.ts";
+
+/** Constructs a promise with {@linkcode T} value */
+export function $promise<T>(promise: T): Promise<T> {
+  return new Promise(resolve=> resolve(promise));
+}
+
+
