df6ad5245f7a24c57d6e3155328b01abb0256aec
Nate River <trishiragolder69@outlook.com>
2024-07-15 02:27:54 +0530

    added all types of typed arrays

--- src/typed_array.rs
+++ src/typed_array.rs
@@ -2,6 +2,15 @@
 use macros::typed_array;
 
 typed_array! {
-  Uint8Vec: u8
+  Uint8Vec: u8,
+  Uint16Vec: u16,
+  Uint32Vec: u32,
+  Uint64Vec: u64,
+  Int8Vec: i8,
+  Int16Vec: i16,
+  Int32Vec: i32,
+  Int64Vec: i64,
+  Float32Vec: f32,
+  Float64Vec: f64
 }
 
