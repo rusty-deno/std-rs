9eeb1b5bd6d242977da44de52eba82aa728be390
Nate River <trishiragolder69@outlook.com>
2024-07-27 00:22:34 +0530

    optimized sub-optimal implementation

--- lib/io/read.ts
+++ lib/io/read.ts
@@ -2,61 +2,32 @@
 import * as lib from "../../bindings/std_rs.js";
 import { Uint8Vec } from './typed_arrays/uint8vec.ts';
 import { $result,$resultSync } from '../error/result/mod.ts';
-import { IoAsyncResult,IoResult,IoErrorKind,IoError } from "./error.ts";
+import { IoAsyncResult,IoResult } from "./error.ts";
 
 
 
 export abstract class Read {
   public abstract read(buf: Uint8Array): IoAsyncResult<number>;
   public abstract readSync(buf: Uint8Array): IoResult<number>;
+  #read=(buf: Uint8Array): Promise<number>=> this.read(buf).unwrapOrThrow();
+  #readSync=(buf: Uint8Array): number=> this.readSync(buf).unwrapOrThrow();
+
 
   public readExact(buf: Uint8Array): IoAsyncResult<void> {
-    return $result(async ()=> {
-      while(!buf.length) {
-        const n=(await this.read(buf)).result;
-
-        if(!n) break;
-        if(typeof n!=="number") {
-          if(n.kind()===IoErrorKind.Interrupted) continue;
-          throw n;
-        }
-        new Uint8Array().slice;
-
-        buf=buf.slice(n);
-      }
-
-      if(buf.length) {
-        throw new IoError(IoErrorKind.UnexpectedEof,"failed to write the whole buffer.");
-      }
-    });
+    const ptr=Deno.UnsafePointer.value(Deno.UnsafePointer.of(buf));
+    return $result(lib.read_exact,this,this.#read,Number(ptr),buf.length);
   }
 
   public readExactSync(buf: Uint8Array): IoResult<void> {
-    return $resultSync(()=> {
-      while(!buf.length) {
-        const n=this.readSync(buf).result;
-
-        if(!n) break;
-        if(typeof n!=="number") {
-          if(n.kind()===IoErrorKind.Interrupted) continue;
-          throw n;
-        }
-
-        buf=buf.slice(n);
-      }
-
-      if(buf.length) {
-        throw new IoError(IoErrorKind.UnexpectedEof,"failed to write the whole buffer.");
-      }
-    });
+    return $resultSync(lib.read_exact_sync,this,this.#readSync,buf);
   }
 
   public readToEnd(buf: Uint8Vec): IoAsyncResult<number> {
-    return $result(lib.read_to_end,buf.thisPtr(),this,this.read);
+    return $result(lib.read_to_end,buf.thisPtr(),this,this.#read);
   }
 
   public readToEndSync(buf: Uint8Vec): IoResult<number> {
-    return $resultSync(lib.read_to_end_sync,buf.thisPtr(),this,this.readSync);
+    return $resultSync(lib.read_to_end_sync,buf.thisPtr(),this,this.#readSync);
   }
 
   // public readToString(): IoAsyncResult<string>;
